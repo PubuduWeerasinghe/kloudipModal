b019cfdcd9ba5efd173f47c3f31e4e6ddfafac13
PubuduLakshan <pubudu.lakshan7@gmail.com>
2025-03-13 18:18:00 +0530

    Popup initial changes

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import Modal from './components/Modal.vue';
+import {ref} from 'vue'
+
+const isOpen = ref(false);
+
+//Open Popup
+const openPopup =()=>{
+  isOpen.value = true;
+}
+
+//Close Popup
+const onClose =()=>{
+  isOpen.value = false;
+}
+</script>
+
+<template>
+ <Modal :isOpen="isOpen" @onClose="onClose"/>
+  <button @click="openPopup"><span style="color: white;">Open Popup Modal</span></button>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="isOpen" class="top-container">
+        <div class="main-container">
+        <h1>Modal Title</h1>
+        <p>Test content</p>
+        <button @click="emits('onClose')">Cancel</button>
+        <button>Confirm</button>
+    </div>
+    </div>
+    
+</template>
+<script lang="ts" setup>
+
+const emits = defineEmits<{onClose: []}>()
+defineProps<{ isOpen; boolean }>()
+
+</script>
+<style scoped>
+.main-container{
+    padding: 20px;
+    background: #fff;
+    border: 2px solid #fff;
+    border-radius: 5px;
+    width: 400px;
+    height: 200px;
+    color: black;
+}
+.top-container{
+    height: 100%;
+    width: 100%;
+    z-index: 9999;
+    position: fixed;
+}
+</style>
